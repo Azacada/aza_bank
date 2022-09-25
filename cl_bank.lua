2df28a4c7aaa8826a27273f61135909ca21cd61e
Azacada <114081147+Azacada@users.noreply.github.com>
2022-09-25 14:55:08 +0200

    Update cl_bank.lua

--- cl_bank.lua
+++ cl_bank.lua
@@ -151,8 +151,6 @@ Banque = {
 } 
 
 
-
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
@@ -169,7 +167,7 @@ Citizen.CreateThread(function()
                 end   
             end
         end
-end
+    end
 end)
 
 
